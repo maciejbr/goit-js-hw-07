361b9536db0969853fc94688793044a6df71daa3
maciejbr <m.broniarski@gmail.com>
2024-07-09 22:18:40 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -4,7 +4,23 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const colorSpan = document.querySelector(".color");
 const button = document.querySelector(".change-color");
-button.addEventListener("click", (event) => {
-  document.body.style.background = getRandomHexColor();
-});
+const handleClick = () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+};
+
+button.addEventListener("click", handleClick);
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// const button = document.querySelector(".change-color");
+// button.addEventListener("click", (event) => {
+//   document.body.style.background = getRandomHexColor();
+// });
